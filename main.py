c53b14dfed3f663706814b155249c558081767f8
Kevin Hosford <kevehosford@gmail.com>
2023-09-03 17:02:15 -0400

    Accept arguments for repo, owner, branch, query so nothing is hardcoded

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import pickle
 import os
+import argparse
 
 from llama_index import download_loader, GPTVectorStoreIndex, StorageContext, load_index_from_storage
 download_loader("GithubRepositoryReader")
 
 from llama_hub.github_repo import GithubClient, GithubRepositoryReader
 
+parser = argparse.ArgumentParser(description="Description of your program")
+
+parser.add_argument("-o", "--owner", type=str, help="Owner of the repository")
+parser.add_argument("-r", "--repo", type=str, help="Name of the repository")
+parser.add_argument("-b", "--branch", type=str, help="Name of the branch", default="main")
+parser.add_argument("-q", "--query", type=str, help="Repo query", default="What's the saddest file in the repo?")
+
+args = parser.parse_args()
+
+owner = args.owner
+repo = args.repo
+branch = args.branch
+query = args.query
+
+print(f"Owner: {owner}")
+print(f"Repo: {repo}")
+print(f"Branch: {branch}")
+print(f"Query: {query}")
+
 docs_file = "docs.pkl"
 index_dir = "index_persist"
-owner = "<owner>"
-repo = "<repo>"
 
 index = None
 
@@ -24,6 +42,10 @@ if index is None:
             docs = pickle.load(f)
 
     if docs is None:
+        # raise error if owner and repo are none
+        if owner is None or repo is None:
+            raise ValueError("Owner and repo cannot be None")
+
         github_client = GithubClient(os.getenv("GITHUB_TOKEN"))
         loader = GithubRepositoryReader(
             github_client,
@@ -35,7 +57,7 @@ if index is None:
             concurrent_requests =    10,
         )
 
-        docs = loader.load_data(branch="main")
+        docs = loader.load_data(branch)
 
         with open(docs_file, "wb") as f:
             pickle.dump(docs, f)
@@ -44,8 +66,6 @@ if index is None:
     index.storage_context.persist(persist_dir=index_dir)
 
 query_engine = index.as_query_engine()
-
-query = "Which is the saddest file. Explain why it is a sad file."
 
 print(query)
 
